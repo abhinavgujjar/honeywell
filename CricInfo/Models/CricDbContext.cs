27d8b378cf1bc5f5560b766ebc406d19758ee242
abhinav <abhinavgujjar@gmail.com>
2014-02-06 12:22:29 +0530

    ef enabled

---
+++ CricInfo/Models/CricDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CricInfo.Models
+{
+    public class CricDbContext : DbContext
+    {
+        public DbSet<Article> Articles { get; set; }
+    }
+}
