0da0f3499c8cfcf445febd1f531349eaa52130c4
abhinav <abhinavgujjar@gmail.com>
2014-02-06 11:55:53 +0530

    articles view

--- CricInfo/Models/Comment.cs
+++ CricInfo/Models/Comment.cs
@@ -8,6 +8,7 @@ namespace CricInfo.Models
     public class Comment
     {
         public int Id { get; set; }
+        public int ArticleId { get; set; }
         public string CommentText { get; set; }
         public DateTime DateCommented { get; set; }
         public int Rating { get; set; }
